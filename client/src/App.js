76cb3522abb1350f8732c2f54bdd82a951747d6f
Samuele Maurizi <samuelemaurizi@gmail.com>
2021-08-22 18:21:51 +0200

    added addExperience and addEducation components

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/Add Education';
 
 import './App.css';
 
@@ -47,6 +49,8 @@ const App = () => {
                 component={CreateProfile}
               />
               <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+              <PrivateRoute exact path='/add-experience' component={AddExperience}/>
+              <PrivateRoute exact path='/add-education' component={AddEducation}/>
             </Switch>
           </section>
         </>
